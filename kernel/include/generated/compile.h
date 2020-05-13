17346dfdee0a12835525fb8a3bef04af775c3e02
arzamas-16 <arzamas-16@hotmail.com>
2020-05-13 03:52:41 +0300

    Added kernel code (2013-07-23)

---
+++ kernel/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Mon Jul 15 00:10:50 CST 2013"
+#define LINUX_COMPILE_BY "android"
+#define LINUX_COMPILE_HOST "builder"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
---
+++ kernel/mediatek/platform/mt6589/preloader/src/init/inc/preloader.h
@@ -0,0 +1,2 @@
+// Auto generated. Build Time Information
+#define BUILD_TIME "20130714-062037"
